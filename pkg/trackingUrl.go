fa431b537f26d7f3f688db2efa1439e83b79a1df
Daniel Wangai <mainad57@gmail.com>
2018-10-30 14:10:24 +0300

    add function to fetch tracking url details

---
+++ pkg/trackingUrl.go
@@ -0,0 +1,40 @@
+package pkg
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+// GetTrackingUrl fetches tracking url details
+// source - https://developer.musixmatch.com/documentation/api-reference/tracking-url-get
+// Required Authentication
+// currently works with format=json
+// TODO - XML
+func GetTrackingURL(domain, format string) (*TrackingURLResponse, error) {
+	trackingUrl := BaseURL + "tracking.url.get"
+	u, err := url.Parse(trackingUrl)
+	if err != nil {
+		return nil, err
+	}
+	q, err := url.ParseQuery(u.RawQuery)
+	if err != nil {
+		return nil, err
+	}
+	q.Add("apikey", APIKEY)
+	q.Add("domain", domain)
+	q.Add("format", format)
+	u.RawQuery = q.Encode()
+	resp, err := http.Get(u.String())
+	if err != nil {
+		return nil, err
+	}
+	var tUrl TrackingURLResponse
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	json.Unmarshal(data, &tUrl)
+	return &tUrl, nil
+}
